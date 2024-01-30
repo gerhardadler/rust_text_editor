7fd596df3e7abcbf61b6741a20ffb43c5065071d
gerhardadler <gerhardskartveit@gmail.com>
2024-01-30 23:12:44 +0100

    cursor rendered correctly when scrolling

--- src/render.rs
+++ src/render.rs
@@ -22,7 +22,10 @@ pub fn render(text_buffer: &TextBuffer, cursor: &Cursor, view: &View) -> io::Res
             .collect::<String>();
         stdout.queue(style::Print(format!("{}\r\n", capped_line)))?;
     }
-    stdout.queue(cursor::MoveTo(cursor.x as u16, cursor.y as u16))?;
+    stdout.queue(cursor::MoveTo(
+        (cursor.x - view.h_scroll) as u16,
+        (cursor.y - view.v_scroll) as u16,
+    ))?;
     stdout.queue(cursor::Show)?;
 
     stdout.flush()?;
